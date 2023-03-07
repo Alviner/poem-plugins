807cc2e85d8690fd9ebe63c729e8bcc5dbe2ecd1
Ivan Sitkin <alviner@yandex-team.ru>
2023-03-07 13:11:25 +0300

    feat: check unset value

--- poem_plugins/config/base.py
+++ poem_plugins/config/base.py
@@ -11,6 +11,10 @@ KwargsType = Mapping[str, Any]
 MapperType = Callable[[Any], Any]
 
 
+def is_unset(value: Any) -> bool:
+    return value in (None, "")
+
+
 class BaseConfig:
     MAPPERS: ClassVar[Mapping[str, MapperType]] = MappingProxyType({})
 
@@ -21,6 +25,9 @@ class BaseConfig:
             mapper = cls.MAPPERS.get(field)
             if not mapper:
                 continue
-            mapped_value = mapper(raw_value)
+            if is_unset(raw_value):
+                mapped_value = None
+            else:
+                mapped_value = mapper(raw_value)
             mapped_kwargs[field] = mapped_value
         return cls(**mapped_kwargs)
